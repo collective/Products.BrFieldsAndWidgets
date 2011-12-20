53432d9ebe862ceb95a2f8e42d8db7e7312d35b6
Érico Andrei <erico@simplesconsultoria.com.br>
2011-12-20 17:00:40 -0200

    Use MessageFactory translation, pep8 compliancy

--- Products/BrFieldsAndWidgets/content/at_mixin.py
+++ Products/BrFieldsAndWidgets/content/at_mixin.py
@@ -15,141 +15,116 @@ schema = Schema((
         name='logradouro',
         widget=StringWidget(
             size="30",
-            description="Exemplo: Rua Mourato Coelho",
-            visible={'view':'invisible', 'edit':'visible'},
-            label='Logradouro',
-            label_msgid='BrFieldsAndWidgets_label_logradouro',
-            description_msgid='BrFieldsAndWidgets_help_logradouro',
-            i18n_domain='Products.BrFieldsAndWidgets',
+            visible={'view': 'invisible', 'edit': 'visible'},
+            label=_(u'Logradouro'),
+            description=_(u'Exemplo: Rua Mourato Coelho'),
         ),
-        required=1,
+        required=True,
         schemata="Address"
     ),
-    
+
     StringField(
         name='numero',
         widget=StringWidget(
             size="7",
-            description="12",
-            visible={'view':'invisible', 'edit':'visible'},
-            label='Número',
-            label_msgid='BrFieldsAndWidgets_label_numero',
-            description_msgid='BrFieldsAndWidgets_help_numero',
-            i18n_domain='Products.BrFieldsAndWidgets',
+            visible={'view': 'invisible', 'edit': 'visible'},
+            label=_(u'Número'),
+            description=_(u'Exemplo: 12'),
         ),
-        required=1,
+        required=True,
         schemata="Address"
     ),
-    
+
     StringField(
         name='complemento',
         widget=StringWidget(
-            visible={'view':'invisible', 'edit':'visible'},
-            label='Complemento',
-            label_msgid='BrFieldsAndWidgets_label_complemento',
-            i18n_domain='Products.BrFieldsAndWidgets',
+            visible={'view': 'invisible', 'edit': 'visible'},
+            label=_(u'Complemento'),
         ),
         schemata="Address"
     ),
-    
+
     StringField(
         name='bairro',
-        widget=StringWidget
-        (
-            description="Exemplo: Pinheiros.",
-            visible={'view':'invisible', 'edit':'visible'},
+        widget=StringWidget(
+            visible={'view': 'invisible', 'edit': 'visible'},
             size="30",
-            label='Bairro',
-            label_msgid='BrFieldsAndWidgets_label_bairro',
-            description_msgid='BrFieldsAndWidgets_help_bairro',
-            i18n_domain='Products.BrFieldsAndWidgets',
+            label=_(u'Bairro'),
+            description=_(u'Exemplo: Pinheiros'),
         ),
-        required=0,
+        required=False,
         schemata="Address",
     ),
-    
+
     CEPField(
         name='cep',
         required=1,
-        widget=CEPWidget(   
-                        description="Informe o CEP.",
-                        visible={'view':'invisible', 'edit':'visible'},
-                        label='CEP',
-                        label_msgid='BrFieldsAndWidgets_label_cep',
-                        description_msgid='BrFieldsAndWidgets_help_cep',
+        widget=CEPWidget(
+                        visible={'view': 'invisible', 'edit': 'visible'},
+                        label=_(u'CEP'),
+                        description=_(u'Informe o CEP.'),
                         ),
         schemata="Address"
     ),
-    
+
     StringField(
         name='cidade',
         widget=StringWidget(
-            description="Informe sua cidade.",
-            visible={'view':'invisible', 'edit':'visible'},
-            label='Cidade',
-            label_msgid='BrFieldsAndWidgets_label_cidade',
-            description_msgid='BrFieldsAndWidgets_help_cidade',
-            i18n_domain='Products.BrFieldsAndWidgets',
+            visible={'view': 'invisible', 'edit': 'visible'},
+            label=_(u'Cidade'),
+            description=_(u'Informe sua cidade.'),
         ),
-        required=1,
+        required=True,
         schemata="Address"
     ),
-    
+
     StringField(
         name='uf',
         widget=SelectionWidget
         (
-            description="Informe seu estado.",
-            visible={'view':'invisible', 'edit':'visible'},
-            size="1",
-            label='Uf',
-            label_msgid='BrFieldsAndWidgets_label_uf',
-            description_msgid='BrFieldsAndWidgets_help_uf',
-            i18n_domain='Products.BrFieldsAndWidgets',
+            visible={'view': 'invisible', 'edit': 'visible'},
+            size=1,
+            label=_(u'UF'),
+            description=_(u'Informe o seu estado.'),
         ),
-        required=1,
+        required=True,
         schemata="Address",
         vocabulary='vocLstUF',
-        enforceVocabulary=1
+        enforceVocabulary=True,
     ),
-    
+
     ComputedField(
         name='Endereco',
         expression='context.fmt_endereco()',
         widget=ComputedWidget(
-            visible={'view':'visible', 'edit':'invisible'},
-            label='Endereco',
-            label_msgid='BrFieldsAndWidgets_label_Endereco',
-            i18n_domain='Products.BrFieldsAndWidgets',
+            visible={'view': 'visible', 'edit': 'invisible'},
+            label=_(u'Endereço'),
         ),
         default_output_type='text/x-html-safe',
         schemata="Address",
-        searchable=1
+        searchable=True
     ),
-    
+
     TextField(
         name='referencia',
-        allowable_content_types=('text/html',),
+        allowable_content_types=('text/html', ),
         widget=RichWidget
         (
-            description="Informe pontos de referência.",
-            visible={'view':'invisible', 'edit':'invisible'},
-            label='Referencia',
-            label_msgid='BrFieldsAndWidgets_label_referencia',
-            description_msgid='BrFieldsAndWidgets_help_referencia',
-            i18n_domain='Products.BrFieldsAndWidgets',
+            visible={'view': 'invisible', 'edit': 'invisible'},
+            label=_(u'Referência'),
+            description=_(u'Informe pontos de referência.'),
         ),
-        required=0,
+        required=False,
         schemata="Address",
-        searchable=1,
+        searchable=True,
         default_output_type='text/x-html-safe'
     ),
 
 ),
 )
 
-br_endereco_schema = BaseSchema.copy() + \
-    schema.copy()
+
+br_endereco_schema = BaseSchema.copy() + schema.copy()
 
 
 class br_endereco:
@@ -202,37 +177,38 @@ class br_endereco:
 
 
     security.declarePrivate('vocLstUF')
+
     def vocLstUF(self):
         """Retorna lista de unidades da federacao
         """
         uf = [
-            ('','Selecione'),
-            ('AC',u'Acre'),
-            ('AL',u'Alagoas'),
-            ('AM',u'Amazonas'),
-            ('AP',u'Amapá'),
-            ('BA',u'Bahia'),
-            ('CE',u'Ceará'),
-            ('DF',u'Distrito Federal'),
-            ('ES',u'Espírito Santo'),
-            ('GO',u'Goiás'),
-            ('MA',u'Maranhão'),
-            ('MG',u'Minas Gerais'),
-            ('MS',u'Mato Grosso do Sul'),
-            ('MT',u'Mato Grosso'),
-            ('PA',u'Pará'),
-            ('PB',u'Paraíba'),
-            ('PE',u'Pernambuco'),
-            ('PI',u'Piauí'),
-            ('PR',u'Paraná'),
-            ('RJ',u'Rio de Janeiro'),
-            ('RN',u'Rio Grande do Norte'),
-            ('RO',u'Rondônia'),
-            ('RR',u'Roraima'),
-            ('RS',u'Rio Grande do Sul'),
-            ('SC',u'Santa Catarina'),
-            ('SE',u'Sergipe'),
-            ('SP',u'São Paulo'),
-            ('TO',u'Tocantins')]
+            ('', 'Selecione'),
+            ('AC', u'Acre'),
+            ('AL', u'Alagoas'),
+            ('AM', u'Amazonas'),
+            ('AP', u'Amapá'),
+            ('BA', u'Bahia'),
+            ('CE', u'Ceará'),
+            ('DF', u'Distrito Federal'),
+            ('ES', u'Espírito Santo'),
+            ('GO', u'Goiás'),
+            ('MA', u'Maranhão'),
+            ('MG', u'Minas Gerais'),
+            ('MS', u'Mato Grosso do Sul'),
+            ('MT', u'Mato Grosso'),
+            ('PA', u'Pará'),
+            ('PB', u'Paraíba'),
+            ('PE', u'Pernambuco'),
+            ('PI', u'Piauí'),
+            ('PR', u'Paraná'),
+            ('RJ', u'Rio de Janeiro'),
+            ('RN', u'Rio Grande do Norte'),
+            ('RO', u'Rondônia'),
+            ('RR', u'Roraima'),
+            ('RS', u'Rio Grande do Sul'),
+            ('SC', u'Santa Catarina'),
+            ('SE', u'Sergipe'),
+            ('SP', u'São Paulo'),
+            ('TO', u'Tocantins')]
 
         return uf
