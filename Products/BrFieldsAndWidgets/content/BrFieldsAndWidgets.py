73a29df77381483cf0bd4e6b34b2cbae2211f4ad
Érico Andrei <erico@simplesconsultoria.com.br>
2011-12-20 17:10:09 -0200

    PEP8 compliance

--- Products/BrFieldsAndWidgets/content/BrFieldsAndWidgets.py
+++ Products/BrFieldsAndWidgets/content/BrFieldsAndWidgets.py
@@ -8,124 +8,120 @@ from Products.Archetypes.Field import StringField
 
 class CPFWidget(StringWidget):
     security = ClassSecurityInfo()
-    
+
     _properties = StringWidget._properties.copy()
     _properties.update({
         'macro': 'BrCPF',
-        'helper_js': ('BrFieldsAndWidgets.js',),
+        'helper_js': ('BrFieldsAndWidgets.js', ),
         })
-    
 
 
 class CPFField(StringField):
-    security  = ClassSecurityInfo()    
+    security = ClassSecurityInfo()
     _properties = StringField._properties.copy()
     _properties.update({
         'widget': CPFWidget,
-        'validators': ('isCPF',),
+        'validators': ('isCPF', ),
         })
-    
 
 
 class CEPWidget(StringWidget):
     security = ClassSecurityInfo()
-    
+
     _properties = StringWidget._properties.copy()
     _properties.update({
         'macro': 'BrCEP',
-        'helper_js': ('BrFieldsAndWidgets.js',),
+        'helper_js': ('BrFieldsAndWidgets.js', ),
         })
-    
 
 
 class CEPField(StringField):
-    security  = ClassSecurityInfo()    
+    security = ClassSecurityInfo()
     _properties = StringField._properties.copy()
     _properties.update({
         'widget': CEPWidget,
-        'validators': ('isCEP',),
+        'validators': ('isCEP', ),
         })
-    
 
 
 class CNPJWidget(StringWidget):
     security = ClassSecurityInfo()
-    
+
     _properties = StringWidget._properties.copy()
     _properties.update({
         'macro': 'BrCNPJ',
-        'helper_js': ('BrFieldsAndWidgets.js',),
+        'helper_js': ('BrFieldsAndWidgets.js', ),
         })
-    
 
 
 class CNPJField(StringField):
-    security  = ClassSecurityInfo()    
+    security = ClassSecurityInfo()
     _properties = StringField._properties.copy()
     _properties.update({
         'widget': CNPJWidget,
-        'validators': ('isCNPJ',),
+        'validators': ('isCNPJ', ),
         })
-    
 
 
 class BrPhoneWidget(StringWidget):
     security = ClassSecurityInfo()
-    
+
     _properties = StringWidget._properties.copy()
     _properties.update({
         'macro': 'BrPhone',
-        'helper_js': ('BrFieldsAndWidgets.js',),
+        'helper_js': ('BrFieldsAndWidgets.js', ),
         })
-    
 
 
 class BrPhoneField(StringField):
-    security  = ClassSecurityInfo()    
+    security = ClassSecurityInfo()
     _properties = StringField._properties.copy()
     _properties.update({
         'widget': BrPhoneWidget,
-        'validators': ('isBrPhone',),
+        'validators': ('isBrPhone', ),
         })
-    
 
 
 class BrTimeWidget(StringWidget):
     security = ClassSecurityInfo()
-    
+
     _properties = StringWidget._properties.copy()
     _properties.update({
         'macro': 'BrTime',
-        'helper_js': ('BrFieldsAndWidgets.js',),
+        'helper_js': ('BrFieldsAndWidgets.js', ),
         })
 
 
 registerWidget(CPFWidget,
                title='CPF',
                description="CPF widget.",
-               used_for=('Products.Archetypes.public.StringField',)
+               used_for=('Products.Archetypes.public.StringField', )
                )
+
 
 registerWidget(CEPWidget,
                title='CEP',
                description="CEP widget.",
-               used_for=('Products.Archetypes.public.StringField',)
+               used_for=('Products.Archetypes.public.StringField', )
                )
+
 
 registerWidget(CNPJWidget,
                title='CNPJ',
                description="CNPJ widget.",
-               used_for=('Products.Archetypes.public.StringField',)
+               used_for=('Products.Archetypes.public.StringField', )
                )
+
 
 registerWidget(BrPhoneWidget,
                title='BrPhone',
                description="BrPhone widget.",
-               used_for=('Products.Archetypes.public.StringField',)
+               used_for=('Products.Archetypes.public.StringField', )
                )
+
 
 registerWidget(BrTimeWidget,
                title='BrTimeWidget',
                description="BrTime widget.",
-               used_for=('Products.Archetypes.public.StringField',)
+               used_for=('Products.Archetypes.public.StringField', )
                )
